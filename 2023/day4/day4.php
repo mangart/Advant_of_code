10165c22eafbd716b86a13ec134a24c36c08d6ba
mangart <zan.gostic1@gmail.com>
2024-11-12 18:37:02 +0100

    AoC day4 part 1 completed

--- 2023/day4/day4.php
+++ 2023/day4/day4.php
@@ -1,14 +1,44 @@
 <?php
 
 function part1($file){
+	$vsota = 0;
 	$lines = file($file);
 	foreach($lines as $line) {
 		$line = trim($line);
+		$line = str_replace("  "," ",$line);
+		$deli = explode("|",$line);
+		if(!isset($deli[1])){
+			return 0;
+		}
+		$myNumbers = array_map("intval",explode(" ",trim($deli[1])));
+		$deli = explode(":",$deli[0]);
+		if(!isset($deli[1])){
+			return 0;
+		}
+		$winningNumbers = array_map("intval",explode(" ",trim($deli[1])));
+		
+		$winning = array();
+		foreach($winningNumbers as $winNum){
+			$winning[$winNum] = 1;
+		}
+		$stev = -1;
+		$myNums = array();
+		foreach($myNumbers as $myNum){
+			$myNums[$myNum] = 1;
+			if(isset($winning[$myNum])){
+				$stev += 1;
+			}
+		}
+
+		if($stev > -1){
+			$vsota += pow(2,$stev);
+		}
 	}
 	echo "Part1 resitev je: ".$vsota."\n";
 }
 
 function part2($file){
+	$vsota = 0;
 	$lines = file($file);
 	foreach($lines as $line) {
 		$line = trim($line);
@@ -16,7 +46,7 @@ function part2($file){
 	echo "Part2 resitev je: ".$vsota."\n";
 }
 
-part1('day1_input.txt');
-part2('day1_input.txt');
+part1('day4_input.txt');
+part2('day4_sample_input.txt');
 ?>
 
