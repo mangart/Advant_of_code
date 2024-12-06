65e2483d065a61a563d6d786eac5e5694c35c482
mangart <zan.gostic1@gmail.com>
2024-12-07 00:55:05 +0100

    added day 5 part2 solution

--- 2024/day5/day5.php
+++ 2024/day5/day5.php
@@ -54,8 +54,7 @@ function part1($file){
 		}
 
 	}
-	echo "Veljavni so: \n";
-	var_dump($valid);
+
 	foreach($valid as $val){
 		$vsota += $val[floor(count($val)/2)];
 	}
@@ -63,21 +62,85 @@ function part1($file){
 }
 
 function part2($file){
-	$vsota = 0;
+$vsota = 0;
 	$lines = file($file);
-	$grid = array();
+	$rules = array();
+	$updates = array();
+	$rule = true;
+	$valid = array();
 	foreach($lines as $line) {
 		$line = trim($line);
-		$line = str_split($line);
-		array_push($grid,$line);
+		if($line == ""){
+			$rule = false;
+		}
+		else if($rule){
+			array_push($rules,$line);
+		} else {
+			array_push($updates,$line);
+		}
+	}
+	//var_dump($rules);
+	//var_dump($updates);
+	
+	$rules_hashmap = array();
+	foreach($rules as $rule){
+		$trenutni = array_map("intval",explode("|",$rule));
+		//var_dump($trenutni);
+		if(isset($rules_hashmap[$trenutni[0]])){
+			$rules_hashmap[$trenutni[0]][$trenutni[1]] = 1;
+		} else {
+			$rules_hashmap[$trenutni[0]] = array();
+			$rules_hashmap[$trenutni[0]][$trenutni[1]] = 1;
+		}
+	}
+	foreach($updates as $upd){
+		$up = array_map("intval",explode(",",$upd));
+		$updates_hashmap = array();
+		for($i = 0;$i < count($up);$i++){
+			$updates_hashmap[$up[$i]] = array();
+			for($j = $i - 1;$j >= 0;$j--){
+				$updates_hashmap[$up[$i]][$up[$j]] = 1; 
+			}
+		}
+		$legit = true;
+		foreach($updates_hashmap as $key => $value){
+			foreach($updates_hashmap[$key] as $podkey => $podvalue){
+				if(isset($rules_hashmap[$key][$podkey])){
+					$legit = false;
+				}
+			}
+		}
+		if(!$legit){
+			array_push($valid,$up);
+		}
+
+	}
+	foreach($valid as $val){
+		for($i = 0;$i < count($val);$i++){
+			for($j = $i - 1;$j >= 0;$j--){
+				if(isset($rules_hashmap[$val[$i]][$val[$j]])){
+					$val = move_back($val,$i,$j);
+					$i = 0;
+				}
+			}
+		}
+		$vsota += $val[floor(count($val)/2)];
 	}
 	
 
 	echo "Part2 resitev je: ".$vsota."\n";
 }
 
+function move_back($val,$i,$j){
+	for($k = $j;$k <= $i && $k + 1 < count($val);$k++){
+		$temp = $val[$k];
+		$val[$k] = $val[$k+1];
+		$val[$k+1] = $temp;
+	}
+	return $val;
+}
 part1('day5_input.txt');
-part2('day5_sample_input.txt');
+part2('day5_input.txt');
 $neki = 7;
 
 ?>
