a0cb6ddb6e17944a8b62ab24609e30d339209de6
mangart <zan.gostic1@gmail.com>
2024-12-20 22:37:03 +0100

    added day6 part 2 solution

--- 2024/day6/day6.php
+++ 2024/day6/day6.php
@@ -1,7 +1,7 @@
 <?php
 
-function part1($file,&$positions){
-	$vsota = 0;
+
+function init($file,&$current_i,&$current_j,&$i_change,&$j_change){
 	$lines = file($file);
 	$grid = array();
 	foreach($lines as $line) {
@@ -20,120 +20,91 @@ function part1($file,&$positions){
 				$current_j = $j;
 				$i_change = -1;
 				$j_change = 0;
+				$grid[$i][$j] = ".";
 				break 2;
 			} else if($grid[$i][$j] == "ˇ"){
 				$current_i = $i;
 				$current_j = $j;
 				$i_change = 1;
 				$j_change = 0;
+				$grid[$i][$j] = ".";
 				break 2;
 			} else if($grid[$i][$j] == ">"){
 				$current_i = $i;
 				$current_j = $j;
 				$i_change = 0;
 				$j_change = 1;
+				$grid[$i][$j] = ".";
 				break 2;
 			} else if($grid[$i][$j] == "<"){
 				$current_i = $i;
 				$current_j = $j;
 				$i_change = 0;
 				$j_change = -1;
+				$grid[$i][$j] = ".";
 				break 2;
 			}
 		}
 	}
+	return $grid;	
+}
+
+function part1($grid,&$positions,&$positions1,$current_i,$current_j,$i_change,$j_change){
+	$vsota = 0;
 	$positions = array();
 	//echo "current i: $current_i, current j: $current_j, change in i: $i_change, change in j: $j_change \n";
 	while(isset($grid[$current_i+$i_change][$current_j+$j_change])){
-		if(isset($positions[$current_i])){
-			$positions[$current_i][$current_j] = 1;
+		array_push($positions1,$current_i.",".$current_j.",".$i_change.",".$j_change);
+		if(isset($positions[$current_i.",".$current_j])){
+			$positions[$current_i.",".$current_j] += 1;
 		} else {
-			$positions[$current_i] = array();
-			$positions[$current_i][$current_j] = 1;
+			$positions[$current_i.",".$current_j] = 1;
 		}
-		if($grid[$current_i+$i_change][$current_j+$j_change] == "#"){
-			if($i_change == -1){
-				$i_change = 0;
-				$j_change = 1;
-			} else if($j_change == 1){
-				$i_change = 1;
-				$j_change = 0;
-			} else if($i_change == 1){
-				$i_change = 0;
-				$j_change = -1;
-			} else if($j_change == -1){
-				$i_change = -1;
-				$j_change = 0;
-			}				
+		$counter = 0;
+		while(isset($grid[$current_i+$i_change][$current_j+$j_change]) && $grid[$current_i+$i_change][$current_j+$j_change] == "#" && $counter < 4){
+			if(isset($grid[$current_i+$i_change][$current_j+$j_change]) && $grid[$current_i+$i_change][$current_j+$j_change] == "#"){
+				if($i_change == -1){
+					$i_change = 0;
+					$j_change = 1;
+				} else if($j_change == 1){
+					$i_change = 1;
+					$j_change = 0;
+				} else if($i_change == 1){
+					$i_change = 0;
+					$j_change = -1;
+				} else if($j_change == -1){
+					$i_change = -1;
+					$j_change = 0;
+				}				
+			}
+			$counter += 1;
 		}
 		$current_i += $i_change;
 		$current_j += $j_change;
 		//echo "curI: $current_i, curJ: $current_j, chI: $i_change, chJ: $j_change \n";
 	}
-	$positions[$current_i][$current_j] = 1;
-	foreach($positions as $pos){
-		$vsota += count($pos);
-	}
+	$positions[$current_i.",".$current_j] = 1;
+	array_push($positions1,$current_i.",".$current_j.",".$i_change.",".$j_change);
+	$vsota += count($positions);
 	echo "Part1 resitev je: ".$vsota."\n";
 }
 
-function part2($file,$positions){
-	$vsota = 0;
-	$lines = file($file);
-	$grid = array();
-	foreach($lines as $line) {
-		$line = trim($line);
-		$line = str_split($line);
-		array_push($grid,$line);
-	}
-	$current_i = 0;
-	$current_j = 0;
-	$i_change = 0;
-	$j_change = 0;
-	for($i = 0;$i < count($grid);$i++){
-		for($j = 0;$j < count($grid[$i]);$j++){
-			if($grid[$i][$j] == "^"){
-				$current_i = $i;
-				$current_j = $j;
-				$i_change = -1;
-				$j_change = 0;
-				break 2;
-			} else if($grid[$i][$j] == "ˇ"){
-				$current_i = $i;
-				$current_j = $j;
-				$i_change = 1;
-				$j_change = 0;
-				break 2;
-			} else if($grid[$i][$j] == ">"){
-				$current_i = $i;
-				$current_j = $j;
-				$i_change = 0;
-				$j_change = 1;
-				break 2;
-			} else if($grid[$i][$j] == "<"){
-				$current_i = $i;
-				$current_j = $j;
-				$i_change = 0;
-				$j_change = -1;
-				break 2;
-			}
-		}
-	}
-	/*for($i = 0;$i < count($grid);$i++){
-		for($j = 0;$j < count($grid[$i]);$j++){*/
-	foreach($positions as $key => $pos){
-		foreach($pos as $subkey => $value){
-			if($grid[$key][$subkey] != "#" && $grid[$key][$subkey] != "^"){
-				$grid[$key][$subkey] = "#";
-				if(cycle($grid,$current_i,$current_j,$i_change,$j_change)){
-					$vsota += 1;
-				}
-				$grid[$key][$subkey] = ".";
-			}
-		}
-	}
 
-	echo "Part2 resitev je: ".$vsota."\n";
+
+function part2($grid,$positions,$current_i,$current_j,$i_change,$j_change){
+	$distinct_positions = array();
+	for($i = 0;$i < count($positions) - 1;$i++){
+		$naslednja = array_map("intval",explode(",",$positions[$i+1]));
+		if(!isset($distinct_positions[$naslednja[0].",".$naslednja[1]])){
+			$grid[$naslednja[0]][$naslednja[1]] = "#";
+			if(cycle($grid,$current_i,$current_j,$i_change,$j_change)){
+				$distinct_positions[$naslednja[0].",".$naslednja[1]] = 1;	
+			}
+			$grid[$naslednja[0]][$naslednja[1]] = ".";
+		}
+	}
+	echo "Part2 resitev je: ".count($distinct_positions)."\n";
+	
 }
 
 function cycle($grid,$current_i,$current_j,$i_change,$j_change){
@@ -145,40 +116,52 @@ function cycle($grid,$current_i,$current_j,$i_change,$j_change){
 		if(isset($positions[$position_key])){
 			//$positions[$current_i][$current_j] = 1;
 			return true;
-		} else {
+		}
 			//$positions[$current_i] = array();
-			$positions[$position_key] = 1;
+		$positions[$position_key] = 1;
+		$counter = 0;
+		while(isset($grid[$current_i+$i_change][$current_j+$j_change]) && $grid[$current_i+$i_change][$current_j+$j_change] == "#" && $counter < 4){
+			if(isset($grid[$current_i+$i_change][$current_j+$j_change]) && $grid[$current_i+$i_change][$current_j+$j_change] == "#"){
+				if($i_change == -1){
+					$i_change = 0;
+					$j_change = 1;
+				} else if($j_change == 1){
+					$i_change = 1;
+					$j_change = 0;
+				} else if($i_change == 1){
+					$i_change = 0;
+					$j_change = -1;
+				} else if($j_change == -1){
+					$i_change = -1;
+					$j_change = 0;
+				}				
+			}
+			$counter += 1;
 		}
-		if(isset($grid[$current_i+$i_change][$current_j+$j_change]) && $grid[$current_i+$i_change][$current_j+$j_change] == "#"){
-			if($i_change == -1){
-				$i_change = 0;
-				$j_change = 1;
-			} else if($j_change == 1){
-				$i_change = 1;
-				$j_change = 0;
-			} else if($i_change == 1){
-				$i_change = 0;
-				$j_change = -1;
-			} else if($j_change == -1){
-				$i_change = -1;
-				$j_change = 0;
-			}				
-		}
+		
 		$current_i += $i_change;
 		$current_j += $j_change;
-		//echo "curI: $current_i, curJ: $current_j, chI: $i_change, chJ: $j_change \n";
+		//echo "curI: $current_i, curJ: $current_j, chI: $i_change, chJ: $j_change \n"; 
 	}
 	//var_dump($positions);
 	return false;
+	
 }
 
 $start = microtime(true);
 $positions = array();
-part1('day6_input.txt',$positions);
+$positions1 = array();
+$current_i = 0;
+$current_j = 0;
+$i_change = 0;
+$j_change = 0;
+$grid = init('day6_input.txt',$current_i,$current_j,$i_change,$j_change);
+part1($grid,$positions,$positions1,$current_i,$current_j,$i_change,$j_change);
 echo ($time_elapsed_secs = microtime(true) - $start)."\n";
 $start = microtime(true);
-part2('day6_input.txt',$positions);
+part2($grid,$positions1,$current_i,$current_j,$i_change,$j_change);
 echo ($time_elapsed_secs = microtime(true) - $start)."\n";
+
 /*foreach($positions as $pos){
 	echo count($pos)."\n";
 }*/
