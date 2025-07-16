85de61d87550f10c99ba6b83dcadfedc2b042b96
mangart <zan.gostic1@gmail.com>
2025-07-17 00:43:06 +0200

    Added day9 part 2 solution

--- 2024/day9/day9.php
+++ 2024/day9/day9.php
@@ -21,6 +21,7 @@ function izpisi_tabelo($disk){
 	}
 	echo "\n";
 }
+
 function part1($disk){
 	$array_disk = array();
 	$id = 0;
@@ -60,10 +61,87 @@ function part1($disk){
 	return $vsota;
 }
 
-
+function izpisi_disk($diskArray){
+	foreach($diskArray as $da){
+		for($i = 0;$i < $da['amount'];$i++){
+			if($da['type'] == 'data'){
+				echo $da['id'];
+			} else {
+				echo ".";
+			}
+		}
+	}
+	echo "\n";
+}
 
 function part2($disk){
-
+	$diskArray = array();
+	$id = 0;
+	for($i = 0;$i < count($disk);$i++){
+		if($i % 2 == 0){
+			$element = array();
+			$element['type'] = 'data';
+			$element['amount'] = $disk[$i];
+			$element['id'] = $id;
+			array_push($diskArray,$element);
+			$id += 1;
+		} else {
+			$element = array();
+			$element['type'] = 'empty';
+			$element['amount'] = $disk[$i];
+			$element['id'] = -1;
+			array_push($diskArray,$element);
+		}
+	}
+	
+	
+	//izpisi_disk($diskArray);
+	
+	for($i = count($diskArray) - 1;$i >= 0;$i--){
+		if($diskArray[$i]['type'] == 'data'){
+			for($j = 0;$j < $i;$j++){
+				if($diskArray[$j]['type'] == 'empty'){
+					if($diskArray[$i]['amount'] == $diskArray[$j]['amount']){
+						$diskArray[$j]['type'] = 'data';
+						$diskArray[$j]['id'] = $diskArray[$i]['id'];
+						
+						$diskArray[$i]['type'] = 'empty';
+						$diskArray[$i]['id'] = -1;
+						break;
+					}
+					if($diskArray[$i]['amount'] < $diskArray[$j]['amount']){
+						$newElement = array();
+						$newElement['type'] = 'empty';
+						$newElement['amount'] = $diskArray[$j]['amount'] - $diskArray[$i]['amount'];
+						$newElement['id'] = -1;
+						$diskArray[$j]['type'] = 'data';
+						$diskArray[$j]['amount'] = $diskArray[$i]['amount'];
+						$diskArray[$j]['id'] = $diskArray[$i]['id'];
+						$diskArray[$i]['type'] = 'empty';
+						$diskArray[$i]['id'] = -1;
+						array_splice($diskArray,$j+1,0,array($newElement));
+						break;
+					}
+				}
+			}
+		}
+	}
+	
+	//izpisi_disk($diskArray);
+	$vsota = 0;
+	$indeks = 0;
+	for($i = 0;$i < count($diskArray);$i++){
+		if($diskArray[$i]['type'] == 'empty'){
+			$indeks += $diskArray[$i]['amount'];
+		} else {
+			for($j = 0;$j < $diskArray[$i]['amount'];$j++){
+				$vsota += $diskArray[$i]['id'] * $indeks;
+				$indeks++;
+			}
+		}
+	}
+	return $vsota;
+	
 				
 }
 
@@ -74,6 +152,6 @@ $disk = init('day9_input.txt');
 echo "The checksum is: ".part1($disk)."\n";
 echo ($time_elapsed_secs = microtime(true) - $start)."\n";
 $start = microtime(true);
-part2($disk);
+echo "The checksum is: ".part2($disk)."\n";
 echo ($time_elapsed_secs = microtime(true) - $start)."\n";
 ?>
