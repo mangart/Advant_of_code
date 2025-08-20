884261db6d15c3dce573a383dd6550b313a4923f
mangart <zan.gostic1@gmail.com>
2025-08-20 17:32:08 +0200

    added day 12 part 2 solution

--- 2024/day12/day12.php
+++ 2024/day12/day12.php
@@ -84,6 +84,94 @@ function getRegionCount($region,$grid,$symbol){
 	return $count;
 }
 
+function getEdges($region,$grid,$symbol){
+	$edges = array();
+	$count = 0;
+	foreach($region as $reg){
+		if(isset($grid[$reg[0]-1][$reg[1]])){
+			if($grid[$reg[0]-1][$reg[1]] != $symbol){
+				if(!isset($edges["N"][$reg[0]-1])){
+					$edges["N"][$reg[0]-1] = array($reg[1]);
+				} else {
+					array_push($edges["N"][$reg[0]-1],$reg[1]);
+				}
+			}
+		} else {
+			if(!isset($edges["N"][$reg[0]-1])){
+					$edges["N"][$reg[0]-1] = array($reg[1]);
+				} else {
+					array_push($edges["N"][$reg[0]-1],$reg[1]);
+				}
+		}
+		if(isset($grid[$reg[0]+1][$reg[1]])){
+			if($grid[$reg[0]+1][$reg[1]] != $symbol){
+				if(!isset($edges["S"][$reg[0]+1])){
+					$edges["S"][$reg[0]+1] = array($reg[1]);
+				} else {
+					array_push($edges["S"][$reg[0]+1],$reg[1]);
+				}
+			}
+		} else {
+			if(!isset($edges["S"][$reg[0]+1])){
+					$edges["S"][$reg[0]+1] = array($reg[1]);
+				} else {
+					array_push($edges["S"][$reg[0]+1],$reg[1]);
+				}
+		}
+		
+		if(isset($grid[$reg[0]][$reg[1]+1])){
+			if($grid[$reg[0]][$reg[1]+1] != $symbol){
+				if(!isset($edges["E"][$reg[1]+1])){
+					$edges["E"][$reg[1]+1] = array($reg[0]);
+				} else {
+					array_push($edges["E"][$reg[1]+1],$reg[0]);
+				}
+			}
+		} else {
+			if(!isset($edges["E"][$reg[1]+1])){
+					$edges["E"][$reg[1]+1] = array($reg[0]);
+				} else {
+					array_push($edges["E"][$reg[1]+1],$reg[0]);
+				}
+		}
+		
+		if(isset($grid[$reg[0]][$reg[1]-1])){
+			if($grid[$reg[0]][$reg[1]-1] != $symbol){
+				if(!isset($edges["W"][$reg[1]-1])){
+					$edges["W"][$reg[1]-1] = array($reg[0]);
+				} else {
+					array_push($edges["W"][$reg[1]-1],$reg[0]);
+				}
+			}
+		} else {
+			if(!isset($edges["W"][$reg[1]-1])){
+					$edges["W"][$reg[1]-1] = array($reg[0]);
+				} else {
+					array_push($edges["W"][$reg[1]-1],$reg[0]);
+				}
+		}			
+	}
+	//var_dump($edges);
+	return $edges;
+}
+
+function getSidesCount($edges){
+	$sides = 0;
+	foreach($edges as $key => $val){
+		foreach($edges[$key] as $edg){
+			sort($edg);
+			for($i = 0;$i < count($edg)-1;$i++){
+				if((abs($edg[$i]-$edg[$i+1])) > 1){
+					$sides += 1;
+				}
+			}
+			$sides += 1;
+		}
+	}
+	return $sides;
+}
+
+
 function part1($grid){
 	$regions = array();
 	$taken = array();
@@ -102,27 +190,52 @@ function part1($grid){
 	}
 	foreach($regions as $key => $region){
 		$neki = explode(":",$key);
-		echo "Region ".$neki[0].": ".count($region)."\n";
 		$regionCount = getRegionCount($region,$grid,$neki[0]);
-		echo " - RegionCount: ".$regionCount."\n";
 		$vsota += count($region) * $regionCount;
 	}
 	return $vsota;
 }
 
-function part2(){
-				
+function part2($grid){
+	$regions = array();
+	$taken = array();
+	$counter = 0;
+	$vsota = 0;
+	for($i = 0;$i < count($grid);$i++){
+		for($j = 0;$j < count($grid[$i]);$j++){
+			if(!isset($taken[$i][$j])){
+				$symbol = $grid[$i][$j];
+				$id = $symbol.":".$counter;
+				$regions[$id] = array();
+				getRegions($regions,$i,$j,$taken,$grid,$id,$symbol);
+				$counter += 1;
+			}
+		}
+	}
+	foreach($regions as $key => $region){
+		$neki = explode(":",$key);
+		$edges = getEdges($region,$grid,$neki[0]);
+		
+		$sides = getSidesCount($edges);
+
+		$totalCount = count($region) * $sides;
+		$vsota += $totalCount;
+	}
+	return $vsota;			
 }
 
-$start = microtime(true);
+
 $grid = init('day12_input.txt');
-var_dump($grid);
+
 $dict = array();
 
+$start = microtime(true);
 echo "The checksum is: ".part1($grid)."\n";
 echo ($time_elapsed_secs = microtime(true) - $start)."\n";
 
 $start = microtime(true);
-echo "The checksum is: ".part2()."\n";
+echo "The checksum is: ".part2($grid)."\n";
 echo ($time_elapsed_secs = microtime(true) - $start)."\n";
+
+
 ?>
