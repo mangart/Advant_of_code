2f456e5d557b646e82dae45fce461208af040eb1
mangart <zan.gostic1@gmail.com>
2025-10-19 00:01:19 +0200

    added AoC 2024 day 18 part 1 and 2 solution

--- 2024/day18/day18.php
+++ 2024/day18/day18.php
@@ -1,26 +1,18 @@
 <?php
 
 // function for initialization and construction of the grid from input file
-function init($file,&$regs,&$prog,&$proStr){
+function init($file,$size,&$grid,&$cords){
 	$lines = file($file);
-	$phase = 0;
+	$grid = array();
+	for($i = 0;$i < $size;$i++){
+		$str = str_repeat(".",$size);
+		$str = str_split($str);
+		array_push($grid,$str);
+	}
+	
 	foreach($lines as $line) {
 		$line = trim($line);
-		if($line == ""){
-			$phase = 1;
-			continue;
-		}
-		if($phase == 0){
-			$reg = explode(":",$line);
-			array_push($regs,intval(trim($reg[1])));
-		} else {
-			$pro = explode(":",$line);
-			$pro = trim($pro[1]);
-			$proStr = $pro;
-			$prog = array_map("intval",explode(",",$pro));
-		}
-		$line = str_split($line);
-		//array_push($grid,$line);
+		array_push($cords,$line);
 	}
 }
 
@@ -28,32 +20,81 @@ function init($file,&$regs,&$prog,&$proStr){
 
 
 
-
-
-function part1($regA,$regB,$regC,$prog){
-
+function part1SetUp(&$grid,$cords,$simBytes){
+	for($i = 0;$i < $simBytes;$i++){
+		$xy = array_map("intval",explode(",",$cords[$i]));
+		$grid[$xy[1]][$xy[0]] = "#";
+	}
 }
 
 
-function part2(){
+function part1($grid,$gridSize){
+	$visited = array();
+	$directions = array(array(-1,0),array(0,-1),array(1,0),array(0,1));
+	$queue = new SplQueue();
+
+	$queue->enqueue([0,0,0]); // x, y, distance
+	$visited["0:0"] = true;
+	
+	while(!$queue->isEmpty()){
+		[$i, $j, $dist] = $queue->dequeue();
+		if($i == ($gridSize - 1) && $j == ($gridSize - 1)){
+            return $dist;
+        }
 		
+		foreach($directions as [$di,$dj]){
+			$newi = $i + $di;
+			$newj = $j + $dj;
+			if(isset($grid[$newi][$newj]) && $grid[$newi][$newj] != '#' && !isset($visited[$newi][$newj])){
+                    $visited[$newi][$newj] = true;
+                    $queue->enqueue([$newi, $newj, $dist + 1]);
+            }
+		}
+	}
+	return false;
 }
 
 
 
 
-$regs = array();
-$prog = array();
-$proStr = "";
-init('day18_sample_input.txt',$regs,$prog,$proStr);
+function part2($grid,$cords,$gridSize,$simBytes){
+	for($i = $simBytes;$i < count($cords);$i++){
+		part1SetUp($grid,$cords,$i);
+		if(!part1($grid,$gridSize)){
+			return $cords[$i-1];
+		}
+	}
+	return -1;
+}
+
+
+
+
+$grid = array();
+$cords = array();
+
+$gridSize = 71;
+$simBytes = 1024;
+init('day18_input.txt',$gridSize,$grid,$cords);
+
+//var_dump($cords);
+
+part1SetUp($grid,$cords,$simBytes);
+/*
+for($i = 0;$i < count($grid);$i++){
+	for($j = 0;$j < count($grid[$i]);$j++){
+		echo $grid[$i][$j];
+	}
+	echo "\n";
+}*/
 
 
 $srt = microtime(true);
-echo "The checksum is: ".part1()."\n";
+echo "The checksum is: ".part1($grid,$gridSize)."\n";
 echo ($time_elapsed_secs = microtime(true) - $srt)."\n";
 
 $srt = microtime(true);
-echo "The checksum is: ".part2()."\n";
+echo "The checksum is: ".part2($grid,$cords,$gridSize,$simBytes)."\n";
 echo ($time_elapsed_secs = microtime(true) - $srt)."\n";
 
 ?>
