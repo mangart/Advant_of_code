120100fe75f7d61d2a2b17dc3cb789734ee25515
mangart <zan.gostic1@gmail.com>
2025-10-20 22:13:06 +0200

    added day 19 part 1 and 2 solutions

--- 2024/day19/day19.php
+++ 2024/day19/day19.php
@@ -1,52 +1,84 @@
 <?php
 
-// function for initialization and construction of the grid from input file
-function init($file,&$patterns,&$wants){
-	$lines = file($file);
-	$phase = 0;
-	foreach($lines as $line) {
-		$line = trim($line);
-		if($line == ""){
-			$phase = 1;
-			continue;
-		}
-		if($phase == 0){
-			$line = explode(",",$line);
-			foreach($line as $l){
-				array_push($patterns,trim($l));
-			}
-		}
-		if($phase == 1){
-			array_push($wants,$line);
-		}
+function init($file, &$patterns, &$wants) {
+    $lines = file($file);
+    $phase = 0;
+    foreach ($lines as $line) {
+        $line = trim($line);
+        if ($line == "") {
+            $phase = 1;
+            continue;
+        }
+        if ($phase == 0) {
+            $line = explode(",", $line);
+            foreach ($line as $l) {
+                $patterns[] = trim($l);
+            }
+        } else {
+            $wants[] = $line;
+        }
+    }
+}
+
+// calculate way to construct a design
+function makeDesign($patterns,$want,$pos,&$memo) {
+    $n = strlen($want);
+
+	// return 1 if we reach the end of the string
+    if($pos == $n){
+		return 1;
 	}
+
+    // if the count for the length is already computed we return it
+    if(isset($memo[$pos])){
+		return $memo[$pos];
+	}
+
+    $count = 0;
+    foreach ($patterns as $pattern) {
+        $len = strlen($pattern);
+        if((($pos + $len) <= $n) && (substr($want, $pos, $len) === $pattern)){
+            $count += makeDesign($patterns, $want, $pos + $len, $memo);
+        }
+    }
+
+    $memo[$pos] = $count;
+    return $count;
+}
+
+// we compute the valid designes (designes we can construct) and for each of them we also store their count of ways we can make a design (for part 2)
+function part1($patterns, $wants, &$valid) {
+    foreach ($wants as $want) {
+        $memo = [];
+        $ways = makeDesign($patterns, $want, 0, $memo);
+        if ($ways > 0) {
+            $valid[$want] = $ways;
+        }
+    }
+    return count($valid);
 }
 
 
-
-$patterns = array();
-$wants = array();
-
-function part1(){
-
+// since we already have the counts from part 1 we just sum over them
+function part2($valid){
+	$vsota = 0;
+	foreach($valid as $val){
+		$vsota += $val;
+	}
+	return $vsota;
 }
 
+$patterns = [];
+$wants = [];
+init('day19_input.txt', $patterns, $wants);
 
-function part2(){
-	
-}
-
-
-init('day19_sample_input.txt',$patterns,$wants);
-var_dump($patterns);
-var_dump($wants);
+$valid = [];
+$srt = microtime(true);
+echo "The checksum is: " . part1($patterns, $wants, $valid) . "\n";
+echo (microtime(true) - $srt) . " seconds\n";
 
 $srt = microtime(true);
-echo "The checksum is: ".part1()."\n";
-echo ($time_elapsed_secs = microtime(true) - $srt)."\n";
-
-$srt = microtime(true);
-echo "The checksum is: ".part2()."\n";
-echo ($time_elapsed_secs = microtime(true) - $srt)."\n";
+echo "The checksum is: " . part2($valid) . "\n";
+echo (microtime(true) - $srt) . " seconds\n";
 
 ?>
