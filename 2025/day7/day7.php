c6a2f0053f0637c2631b608d305186a95eadeda6
mangart <zan.gostic1@gmail.com>
2025-12-08 22:16:35 +0100

    addad AoC 2025 day 7 part 1 solution

--- 2025/day7/day7.php
+++ 2025/day7/day7.php
@@ -12,10 +12,48 @@ function init($file) {
 	return $grid;
 }
 
+function findStart($grid){
+	for($i = 0;$i < count($grid);$i++){
+		for($j = 0;$j < count($grid[$i]);$j++){
+			if($grid[$i][$j] == "S"){
+				return "$i:$j";
+			}
+		}
+	}
+	return false;
+}
+
+function findSplits($grid,&$splits,&$visited,$startI,$startJ){
+	for($i = $startI+1;$i < count($grid);$i++){
+		if(!isset($grid[$i][$startJ])){
+			return true;
+		}
+		if(isset($grid[$i][$startJ]) && $grid[$i][$startJ] == "^"){
+			$splits[$i.":".$startJ] = 1;
+			
+			if(isset($grid[$i][$startJ-1]) && !isset($visited[$i.":".($startJ-1)])){
+				$visited[$i.":".($startJ-1)] = 1;
+				findSplits($grid,$splits,$visited,$i,$startJ-1);
+			}
+			if(isset($grid[$i][$startJ+1]) && !isset($visited[$i.":".($startJ+1)])){
+				$visited[$i.":".($startJ+1)] = 1;
+				findSplits($grid,$splits,$visited,$i,$startJ+1);
+			}
+			return true;
+		}
+	}
+	return true;
+}
+
 function part1($grid){
 	$vsota = 0;
-	
-	return $vsota;
+	$splits = array();
+	$start = explode(":",findStart($grid));
+	$startI = intval($start[0]);
+	$startJ = intval($start[1]);
+	$visited = array();
+	findSplits($grid,$splits,$visited,$startI,$startJ);
+	return count($splits);
 }
 
 function part2($grid){
@@ -24,8 +62,8 @@ function part2($grid){
 	return $vsota;
 }
 
-$grid = init('day7_sample_input.txt');
-var_dump($grid);
+$grid = init('day7_input.txt');
+
 
 $srt = microtime(true);
 echo "The checksum for part1 is: ".part1($grid)."\n";
